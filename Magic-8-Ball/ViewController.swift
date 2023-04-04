ac113b7435506fcd8bf2cc72e83e6c4915e79d60
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2023-04-04 16:12:21 -0700

    one more thing

--- Magic-8-Ball/ViewController.swift
+++ Magic-8-Ball/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Properties
+        var answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var shakeButton: UIButton!
 
+    @IBAction func uibutton(_ sender: Any) {
+            generateAnswer()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+            guard motion == .motionShake else { return }
+
+            generateAnswer()
+    }
+    
+    func generateAnswer() {
+        let randomIndex = Int.random(in: 0..<answers.count)
+
+        answerLabel.text = answers[randomIndex]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        generateAnswer()
+    }
+    
 }
 
